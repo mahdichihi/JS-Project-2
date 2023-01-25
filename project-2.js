3268fc2728e467e37c39e294bae5964e4af7efe0
mahdi chihi <mahdichihi95@gmail.com>
2023-01-25 19:58:37 +0100

    Project Completed : Modal animation

--- project-2.js
+++ project-2.js
@@ -1,1 +1,27 @@
 // Project 2
+
+// Variables
+
+let openBtn = document.getElementById("open-btn");
+let modalContainer = document.getElementById("modal-container");
+let closeBtn = document.getElementById("close-btn");
+
+// Event Listener
+
+// Open Modal
+
+openBtn.addEventListener("click", function () {
+    modalContainer.style.display = "block";
+});
+
+// Close Modal
+
+closeBtn.addEventListener("click", function () {
+    modalContainer.style.display = "none";
+});
+
+window.addEventListener("click", function (e) {
+    if (e.target === modalContainer) {
+        modalContainer.style.display = "none";
+    }
+});
