9899649d2c55b55aac096afc3154d7cc6eeae268
mahdi chihi <mahdichihi95@gmail.com>
2023-01-25 19:45:21 +0100

    Project Completed : Modal animation

--- project-2.js
+++ project-2.js
@@ -1,1 +1,67 @@
 // Project 2
+
+// Variables
+
+let openBtn = document.getElementById("open-btn");
+let modalContainer = document.getElementById("modal-container");
+let closeBtn = document.getElementById("close-btn");
+
+// Event Listener
+
+// Open Modal
+
+openBtn.addEventListener("click", function () {
+    modalContainer.style.display = "block";
+});
+
+// Close Modal
+
+closeBtn.addEventListener("click", function () {
+    modalContainer.style.display = "none";
+});
+
+window.addEventListener("click", function (e) {
+    console.log(e.target);
+    if (e.target === modalContainer) {
+        modalContainer.style.display = "none";
+    }
+});
+// const quotes = [
+//     {
+//         quote: '"The best way to find yourself is to lose yourself in the service of others."',
+//         person: "Mahatma Gandhi",
+//     },
+//     {
+//         quote: '"if you want to live a happy life, tie it to goal, not to people or things."',
+//         person: "Albert Einstein",
+//     },
+//     {
+//         quote: '"At his best, man is the noblest of all animals; separated from law and justice he is the worst."',
+//         person: "Aristotle",
+//     },
+
+//     {
+//         quote: "\"Your time is limited, so don't waste it living someone else's life.\"",
+//         person: "Steve Jobs",
+//     },
+//     {
+//         quote: '"It doesn\'t matter how slowly you go as long as you do not stop"',
+//         person: "Confucius",
+//     },
+// ];
+
+// let previousRandom;
+// btn.addEventListener("click", function () {
+//     let random = Math.floor(Math.random() * quotes.length);
+
+//     // check if the new random number is the same as the previous one
+//     while (random === previousRandom) {
+//         random = Math.floor(Math.random() * quotes.length);
+//     }
+
+//     quote.innerText = quotes[random].quote;
+//     person.innerText = quotes[random].person;
+
+//     // update previousRandom variable
+//     previousRandom = random;
+// });
